1c16fd2b5c94f89ccfa2fd07493db4c7d1cb2383
michaeldoye <michaeldoye@gmail.com>
2013-07-04 11:26:46 +0200

    More Comments

--- globals.php
+++ globals.php
@@ -1,9 +1,12 @@
 <?php 
+///Define some vars based on user input to be used to fetch the DB's///
+
 	//Draggable
 	$dbname = $_GET["fname"];
 	$user = $_GET["user"];
 	$pass = $_GET["pass"];
 	$tablename = $_GET["tablename"];
+	
 	//Droppable
 	$dbname1 = $_GET["fname1"];
 	$user1 = $_GET["user1"];
--- index.php
+++ index.php
@@ -5,20 +5,22 @@
 	<?php include 'header.php'; ?>
  
  
-<!-- Main Page Content -->
+<!-- Start Main Page Content -->
  <div class="row">
 	<div class="six columns canvas"><div class="heading">Table: <?php echo $tablename ; ?><span class="hright"><i class="gen-enclosed heading-tip foundicon-tools"></i><i class="gen-enclosed heading-tip foundicon-globe"></i><i class="gen-enclosed heading-tip foundicon-cloud"></i></span></div>
  
 <!-- Include the draggable loop -->
 	<?php include 'draggable.php';?>
 	
- </div>
+	</div>
 	<div class="six columns canvas"><div class="heading">Table: <?php echo $tablename1 ; ?><span class="hright"><i class="gen-enclosed heading-tip foundicon-tools"></i><i class="gen-enclosed heading-tip foundicon-globe"></i><i class="gen-enclosed heading-tip foundicon-cloud"></i></span></div>
  
 <!-- Include the droppble loop -->
 	<?php include 'droppable.php'; ?>
 	
 	</div>
+	
+	<!-- End Main Page Content -->
  </div>
     </div> 
 
